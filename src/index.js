a9867f2fbd42450c72287e14346af7a4e94fa312
Yago <almeidayago167@gmail.com>
2024-05-08 16:53:43 -0400

    complete TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskBuilder(e.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function taskBuilder(tasks){
+  let p = document.createElement("p");
+  p.textContent = `${tasks} `;
+  let select = document.querySelector("select")
+  p.className = select.value
+  document.querySelector("#tasks").appendChild(p);
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteButton)
+  btn.textContent = "x";
+  p.appendChild(btn)
+}
+
+function deleteButton(event){
+  event.target.parentNode.remove()
+}
+
+function dropDownPriority(){
+  let select = document.createElement("select");
+  document.getElementById("create-task-form").append(select);
+
+}
